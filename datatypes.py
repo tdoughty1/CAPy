668791a92ab83b93d0dcba50ec5e2da823e20932
Todd Doughty <tdoughty1@berkeley.edu>
2014-07-15 14:16:49 -0700

    Continuing docstring docstring documentation, simplifying code

--- datatypes.py
+++ datatypes.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 """
+datatypes.py
+
+Module contains the classes for accessing data/cuts.  Matches the functionality
+of CAP data access routines.
+
+Classes:
+    Data_Access - Class for a data access object
+    Cut_Access - Class for a cut access object. Subclass of Data_Access
+
 Created on Sun Nov  3 14:57:28 2013
 
 @author: tdoughty1
@@ -8,25 +17,45 @@ Created on Sun Nov  3 14:57:28 2013
 import CAPy.Globals
 
 
-class Data(object):
+class Data_Access(object):
+    ''' Class to call data arrays from interactive shell.
+    
+        Modeled after the RQs from CAP, calling one of these 
+    
+        Called:
+        
+        Constructed:
+        
+        Hidden Methods:
+            _SetArgs
+    
+    '''
 
     def __init__(self, name, isgeneral, iscut=False):
+        ''' Constructs the Data Access object used to access data.
+        
+            Parameters:
+                name: (str) - Name of data to read, corresponds to branch name.
+                isgeneral: (bool) - True if it's general data, false if it's
+                    detector specific.
+                iscut: (bool) - True if it's created for a cut value, false if
+                    it's standard data.
+
+            Raises:
+                #TODO - Implement Type checking
+        '''
 
         self.__name__ = name
         self._isgeneral = isgeneral
         self._iscut = iscut
 
-        # For general cut only need one check
-        if(self._isgeneral):
-            self._isloaded = False
-
-        # For detector specific data need to check for each detector
-        else:
-            self._isloaded = []
-            for i in range(15):
-                self._isloaded.append(False)
 
     def __call__(self, *args):
+        ''' Return array of values.
+        
+            #TODO fill in possibilities
+        
+        '''
 
         # First parse optional arguments (ie. detnum,cut)
         self._SetArgs(args)
@@ -98,9 +127,19 @@ class Data(object):
                       "or globals!"
 
 
-class Cut(Data):
+class Cut_Access(Data_Access):
+    ''' 
+    
+        Called:
+        
+        Constructed:
+        
+        Hidden Methods:
+            _SetArgs
+    
+    '''
 
-    # Overwrites Data class method
+    # Overwrite Inherited method
     def _SetArgs(self, args):
 
         # First check if detector is general of detector specific
