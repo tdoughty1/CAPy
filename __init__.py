66b4b3650953c46dc8f7e6847e85ac02924dd1d9
Todd Doughty <tdoughty1@berkeley.edu>
2014-07-15 14:23:03 -0700

    First Commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+__init__.py
+
+Created on Tues Jul 14 14:21:13 2014
+
+@author: tdoughty1
+"""
+
+# Placeholder
---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+setup.py
+
+Created on Tues Jul 14 14:22:45 2014
+
+@author: tdoughty1
+"""
+
+# Placeholder for setup routine
