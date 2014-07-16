1ed311ebfa0d13508b82c4aa9018b5eafa8f771f
Todd Doughty <tdoughty1@berkeley.edu>
2014-07-16 16:27:34 -0700

    Significantly refactored datatypes

--- base/datatypes.py
+++ base/datatypes.py
@@ -14,148 +14,204 @@ Created on Sun Nov  3 14:57:28 2013
 @author: tdoughty1
 """
 
+# Import Standard Libraries
+from warnings import warn
+
+# Import CAPy global settings
 import CAPy_globals
 
+############## Define useful functions for checking arguments #################
+def Check_Detnum(obj, detnum):
+    ''' Check if argument is a valid detector number. '''
+    
+    if not isinstance(detnum, int):
+        warn('WARNING in ' + obj.__name__ + ':\n\tDetnum should be an' + 
+             ' integer. Using last detector number.')
+        return False
 
-class Data_Access(object):
-    ''' Class to call data arrays from interactive shell.
+    if detnum not in CAPy_globals.GetDetnums():
+        warn('WARNING in ' + obj.__name__ + ':\n\t' + str(detnum) + ' is not' +
+             ' a valid detector number. Using last detector number.')
+        return False
     
-        Modeled after the RQs from CAP, calling one of these 
-    
-        Called:
-        
-        Constructed:
-        
-        Hidden Methods:
-            _SetArgs
-    
-    '''
+    return True
 
-    def __init__(self, name, isgeneral, iscut=False):
+
+def Check_Cut(obj, cut):
+    ''' Check if argument is a valid cut. '''
+    
+    #TODO: Implement Cut testing/possibly a class
+    return True
+
+
+############## Define different calling option functions ######################
+def _GenCut(obj, args):
+    ''' Loads the data for a general cut. '''
+
+    # For a general cut, no arguments should be given
+    if len(args) > 0:
+        warn('WARNING in ' + obj.__name__ + ':\n\t' + obj.__name__ + ' takes' +
+             ' no arguments. Ignoring all arguments.','always')
+    
+    # Now call data
+    print '#TODO: Load Data for ' + obj.__name__ + '() here!' 
+
+
+def _DetCut(obj, args):
+    ''' Loads the data for a detector specific cut. '''
+    
+    if len(args) > 1:
+        warn('WARNING in ' + obj.__name__ + ':\n\t' + obj.__name__ + ' takes' +
+             ' a single argument: detector number. Ignoring additional' +
+             ' arguments.', 'always')
+    
+    # If it's not a valid detector number
+    if not Check_Detnum(args[0]):
+        
+        # Get Last Detector number
+        detnum = CAPy_globals.GetLastDetnum
+    
+    # If it is a valid detector number
+    else:
+        detnum = args[0]
+
+        # Store Detector Number
+        CAPy_globals.SetLastDetnum(detnum)
+
+    # Now call data
+    if detnum:
+        warn('WARNING in ' + obj.__name__ + ':\n\t' + 'No detector given and' +
+             ' none stored in globals. Returning nothing', 'always')
+        return None
+    else:
+        print '#TODO: Load Data for ' + obj.__name__ + '(' + str(detnum) + \
+            ') here!'
+
+
+def _GenData(obj, args):
+    ''' Loads the data for a general data value. '''
+    
+    if len(args) > 1:
+        warn('WARNING in ' + obj.__name__ + ':\n\t' + obj.__name__ + ' takes' +
+             'a single argument: cut. Ignoring additional arguments.', 'always')
+
+    # If it's not a valid cut
+    if not Check_Cut(args[0]):
+        
+        # Get Last Cut
+        cut = CAPy_globals.GetLastCut
+    
+    # If it is a valid cut
+    else:
+        cut = args[0]
+        
+        # Store Cut
+        CAPy_globals.SetLastCut(cut)
+
+    # Now call data
+    if cut:
+        print '#TODO: Load Data for ' + obj.__name__ + ' here!'
+    else:
+        print '#TODO: Load Data for ' + obj.__name__ + '(cut) here!'                
+
+def _DetData(obj, args):
+    ''' Loads the data for a detector specific value. '''
+    
+    # If there are more than 2 arguments
+    if len(args) > 2:
+        warn('WARNING in ' + obj.__name__ + ':\n\t' + obj.__name__ + ' takes' +
+             'two arguments: detnum and cut. Ignoring additional arguments.',
+             'always')
+        detnum = args[0]
+        cut = args[1]
+    
+    # If there are just 2 arguments
+    elif len(args) == 2:
+        detnum = args[0]
+        cut = args[1]
+      
+    # If there is one argument
+    elif len(args) == 1:
+        if Check_Detnum(obj, args[0]):
+            detnum = args[0]
+            CAPy_globals.SetLastDetnum(detnum)
+            cut = CAPy_globals.GetLastCut()
+        elif Check_Cut(obj, args[1]):
+            cut = args[0]
+            CAPy_globals.SetLastCut(cut)
+            detnum = CAPy_globals.GetLastDetnum()
+        else:
+            warn('WARNING in ' + obj.__name__ + ':\n\tArgument is neither a'
+                 ' detnum nor a cut. Ignoring argument.', 'always')
+            detnum = CAPy_globals.GetLastDetnum()
+            cut = CAPy_globals.GetLastCut()
+    
+    # If there are no arguments:
+    else:
+        detnum = CAPy_globals.GetLastDetnum()
+        cut = CAPy_globals.GetLastCut()
+    
+    # Now call data
+    if detnum:
+        if cut:
+            print '#TODO: Load Data for ' + obj.__name__ + ' here!'
+        else:
+            print '#TODO: Load Data for ' + obj.__name__ + '(cut) here!'
+    else:
+        warn('WARNING in ' + obj.__name__ + ':\n\t' + 'No detector given and' +
+             ' none stored in globals. Returning nothing', 'always')
+        return None
+
+# Store functions into a dict for convenience in accessing
+Data_Functions = {'GenCut': _GenCut,
+                  'DetCut': _DetCut,
+                  'GenData': _GenData,
+                  'DetData': _DetData}
+
+class Data_Function(object):
+    
+    def __init__(self, name):
         ''' Constructs the Data Access object used to access data.
         
             Parameters:
                 name: (str) - Name of data to read, corresponds to branch name.
-                isgeneral: (bool) - True if it's general data, false if it's
-                    detector specific.
-                iscut: (bool) - True if it's created for a cut value, false if
-                    it's standard data.
 
             Raises:
                 TypeError: If expected types of arguments doesn't match given.
-                ValueError:
-                    - If name isn't key in the dict of data or cut branch names.
-                    - If isgeneral doesn't match the type determined from files.
-                    - If iscut doesn't match the type determinted from files.
+                ValueError: If name isn't key in the dict of data or cut branch
+                    names.
         '''
         
         if not isinstance(name, str):
             raise TypeError('ERROR in Data_Array():\n' +
                             'Name must be a string!')
 
-        if name is in globals.fInfo
-
+        # Store name and check if its a global (only detnum = 1) value
         self.__name__ = name
-        self._isgeneral = isgeneral
-        self._iscut = iscut
+        #self._isgeneral = CAPy_globals.IsGeneral(name)
 
-
-    def __call__(self, *args):
-        ''' Return array of values.
-        
-            #TODO fill in possibilities
-        
         '''
-
-        # First parse optional arguments (ie. detnum,cut)
-        self._SetArgs(args)
-
-
-    # Standard Get Data function should be good for RQs and RRQs
-    # Need to overwrite in Cut class
-    def _SetArgs(self, args):
-
-        # Argument Parsing
-
-        # First check if data is general or detector specific
-        # For general data, take at most one argument: a cut
-        # Store cut in globals module
-        if(self._isgeneral):
-            if(len(args) > 1):
-                print self.__name__ + ' takes at most one argument (cut).'
-                print 'Ignoring additional arguments.'
-                globals.Set_LastCut(args[0])
-            elif(len(args) == 1):
-                globals.Set_LastCut(args[0])
-
-            # Now check to confirm that cut is defined
-            if(globals.Get_LastCut is None):
-                print "Error in " + self.__name__ + ":"
-                print "Cut must be either assigned in call, or globals!"
-
-        # For detector specific data take at most two arguments: detnum and cut
-        # Store detnum and cut in globals module
+        # Set Key Flag for general status
+        if self._isgeneral:
+            key1 = 'Gen'
         else:
-            if(len(args) > 2):
-                print self.__name__ + ' takes at most two arguments' + \
-                                      ' (detnum,cut).'
-                print 'Ignoring additional arguments.'
-                globals.Set_LastDetnum(args[0])
-                globals.Set_LastCut(args[1])
-            elif(len(args) == 2):
-                globals.Set_LastDetnum(args[0])
-                globals.Set_LastCut(args[1])
-            elif(len(args) == 1):
+            key1 = 'Det'
 
-                # There is some ambiguity here, is it a detnum or a cut
-                # Treat it as a detector number if it's in the global list
-                if(args[0] in globals._Detnums):
-                    globals.Set_LastDetnum(args[0])
-
-                # Otherwise it must be a cut, so store as such
-                else:
-                    globals.Set_LastCut(args[0])
-
-            # Now check to confirm that both cut and detnum are defined
-            if(globals.Set_LastDetnum is None or
-               globals.Set_LastCut is None):
-                print "Error in " + self.__name__ + ":"
-                print "Detnum and cut must be either assigned in call, " + \
-                      "or globals!"
-
-
-class Cut_Access(Data_Access):
-    ''' 
-    
-        Called:
-        
-        Constructed:
-        
-        Hidden Methods:
-            _SetArgs
-    
-    '''
-
-    # Overwrite Inherited method
-    def _SetArgs(self, args):
-
-        # First check if detector is general of detector specific
-        # For general cuts, take no arguments
-        if(self._isgeneral):
-            if(len(args) > 0):
-                print self.__name__ + ' takes no arguments.'
-                print 'Ignoring any arguments.'
-
-        # For detector specific cuts take at most one argument: detnum
+        # Check if it's a cut or data
+        if name in CAPy_globals.GetDataNames():
+            self._iscut = False
+            key2 = 'Data'
+        elif name in CAPy_globals.GetCutNames():
+            self._iscut = True
+            key2 = 'Cut'
         else:
-            if(len(args) > 1):
-                print self.__name__ + ' takes at most one argument (detnum).'
-                print 'Ignoring additional arguments.'
-                globals.Set_LastDetnum(args[0])
-            elif(len(args) == 1):
-                globals.Set_LastDetnum(args[0])
-
-            # Now check to confirm that detnum is defined
-            if(globals.Set_LastDetnum is None):
-                print "Error in " + self.__name__ + ":"
-                print "Detnum must be either assigned in call, or in globals!"
+            raise ValueError('ERROR in Data_Array():\n' + 
+                             name + ' not loaded into the current session!')
+        '''
+        
+        # Assign appropriate check function
+        self._CallFunc = Data_Functions['GenCut']
+                     
+    def __call__(self, *args):
+        
+        return self._CallFunc(self, *args)
