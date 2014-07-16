2cce3f2499b0f3a8cc95a63f7d2e5e4c1f6e3fd1
tdoughty1 <tdoughty1@berkeley.edu>
2014-07-15 21:19:08 -0700

    Changed globals access and attributes

--- datatypes.py
+++ datatypes.py
@@ -14,7 +14,7 @@ Created on Sun Nov  3 14:57:28 2013
 @author: tdoughty1
 """
 
-import CAPy.Globals
+import globals
 
 
 class Data_Access(object):
@@ -62,17 +62,6 @@ class Data_Access(object):
 
         # Next check if data is currently stored
         # First check the Cut or DataBuffer
-        if(self._iscut):
-            pass
-        else:
-            if(self._isgeneral):
-                return CAPy.Globals._DataManager.ReadData(self.__name__)
-            else:
-                detnum = CAPy.Globals.GetDetnum()
-                return CAPy.Globals._DataManager.ReadData(self.__name__,
-                                                          detnum)
-
-            # If you make it here, it's the first time loading, so load data
 
     # Standard Get Data function should be good for RQs and RRQs
     # Need to overwrite in Cut class
@@ -87,12 +76,12 @@ class Data_Access(object):
             if(len(args) > 1):
                 print self.__name__ + ' takes at most one argument (cut).'
                 print 'Ignoring additional arguments.'
-                CAPy.Globals.SetCut(args[0])
+                globals.Set_LastCut(args[0])
             elif(len(args) == 1):
-                CAPy.Globals.SetCut(args[0])
+                globals.Set_LastCut(args[0])
 
             # Now check to confirm that cut is defined
-            if(CAPy.Globals.SetCut is None):
+            if(globals.Get_LastCut is None):
                 print "Error in " + self.__name__ + ":"
                 print "Cut must be either assigned in call, or globals!"
 
@@ -103,25 +92,25 @@ class Data_Access(object):
                 print self.__name__ + ' takes at most two arguments' + \
                                       ' (detnum,cut).'
                 print 'Ignoring additional arguments.'
-                CAPy.Globals.SetDetnum(args[0])
-                CAPy.Globals.SetCut(args[1])
+                globals.Set_LastDetnum(args[0])
+                globals.Set_LastCut(args[1])
             elif(len(args) == 2):
-                CAPy.Globals.SetDetnum(args[0])
-                CAPy.Globals.SetCut(args[1])
+                globals.Set_LastDetnum(args[0])
+                globals.Set_LastCut(args[1])
             elif(len(args) == 1):
 
                 # There is some ambiguity here, is it a detnum or a cut
                 # Treat it as a detector number if it's in the global list
-                if(args[0] in CAPy.Globals._Detnums):
-                    CAPy.Globals.SetDetnum(args[0])
+                if(args[0] in globals._Detnums):
+                    globals.Set_LastDetnum(args[0])
 
                 # Otherwise it must be a cut, so store as such
                 else:
-                    CAPy.Globals.SetCut(args[0])
+                    globals.Set_LastCut(args[0])
 
             # Now check to confirm that both cut and detnum are defined
-            if(CAPy.Globals.SetDetnum is None or
-               CAPy.Globals.SetCut is None):
+            if(globals.Set_LastDetnum is None or
+               globals.Set_LastCut is None):
                 print "Error in " + self.__name__ + ":"
                 print "Detnum and cut must be either assigned in call, " + \
                       "or globals!"
@@ -154,11 +143,11 @@ class Cut_Access(Data_Access):
             if(len(args) > 1):
                 print self.__name__ + ' takes at most one argument (detnum).'
                 print 'Ignoring additional arguments.'
-                CAPy.Globals.SetDetnum(args[0])
+                globals.Set_LastDetnum(args[0])
             elif(len(args) == 1):
-                CAPy.Globals.SetDetnum(args[0])
+                globals.Set_LastDetnum(args[0])
 
             # Now check to confirm that detnum is defined
-            if(CAPy.Globals.SetDetnum is None):
+            if(globals.Set_LastDetnum is None):
                 print "Error in " + self.__name__ + ":"
                 print "Detnum must be either assigned in call, or in globals!"
