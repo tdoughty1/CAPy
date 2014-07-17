fef6c40d37fc4939aef4ea7ad4aa3265abfdb247
Todd Doughty <tdoughty1@berkeley.edu>
2014-07-17 15:57:55 -0700

    Changed name of CAPy_globals call

--- CAPy.py
+++ CAPy.py
@@ -10,7 +10,7 @@ from base.fileinfo import FileInfo
 from base.datatypes import Data_Function
 
 # Import Global session variables
-import base.CAPy_globals
+import base.CAPy_globals as CAPy_globals
 
 def Start_Session(fileList):
     
